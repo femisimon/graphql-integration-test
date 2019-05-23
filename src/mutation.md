c3f90389804e9253540b6453f88778db82968c2d
Femi Alashi <femisimon@gmail.com>
2019-05-23 16:39:00 -0700

    graphql playbook, server and mutation

---
+++ src/mutation.md
@@ -0,0 +1,23 @@
+## Mutation
+### What is Mutation
+The Mutation type is the entry point into our graph for modifying data.
+The Mutation type is a special object type, just like the Query type.
+The response of a Mutation depends on the project and should be an interface but this introduction,
+We will define the response to just something simple.
+
+## ApolloServer
+### Starting ApolloServer
+- Lets go back to the /src/index.js file and see what we did to start the ApolloServer
+  - We go the type definition from the schema that we defined which has the Query, Mutation, Scalar and object types.
+  - We imported the `gql` function from `apollo-server` and define all the needed type definitions.
+  - We imported the `ApolloServer` function from `apollo-server` and created a new instance of the server.
+    - Started the server with the type definition from our schema.
+    - Start your GraphQLPlaybook and start the server with `npm start` or `npm i && npm start` if you have not installed packages yet.
+    - Type the code below in your GraphQLPlaybook, you should return data with null object
+    ```
+    {
+      launch(id: 1){
+        mission_name
+      }
+      }
+      ```
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server');
 
-const typeDefs = gql``;
+
+const typeDefs = gql`
 
 type Launch {
   flight_number: ID!
@@ -8,5 +9,70 @@ type Launch {
   ships: Ship
   rocket: Rocket
 }
+
+# A query is an entry point for our schema and what that means is how we access the data we requested for.
+type Query {
+  launches: [Launch]!
+  launch(id: ID!): Launch
+  me: User
+}
+
+"""
+The object types below all have scalar types.
+Scalar types are primitive type and in GraphQL are ID, String, Boolean, Float and Int.
+More on types here => https://devhints.io/graphql#schema
+
+Notice below as Mission has a field - missionPatch which accepts arguements.
+GraphQL is so flexible that it allows that.
+"""
+
+type Rocket {
+  id: ID!
+  name: String
+  type: String
+}
+
+type Ship {
+  ship_id: String
+  ship_name: String
+  ship_type: String
+  active: Boolean
+}
+
+type User {
+  id: ID!
+  email: String!
+  trips: [Launch]!
+}
+
+type Mission {
+  name: String
+  missionPatch(size: PatchSize): String
+}
+
+enum PatchSize {
+  SMALL
+  LARGE
+}
+
+# Mutation type Definition
+
+type Mutation {
+# if false, booking trips failed -- check errors
+  bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  # if false, cancellation failed -- check errors
+  cancelTrip(launchId: ID!): TripUpdateResponse!
+
+  login(email: String): String # login token
+}
+
+# Lets define the TripUpdateResponse which cannot be null
+
+type TripUpdateResponse {
+  success: Boolean!
+  message: String
+  launches: [Launch]
+}`;
+
 
 module.exports = typeDefs;
